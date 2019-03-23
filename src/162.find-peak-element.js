590bc61db08d9155cb6dcefe1f05d8086bd662e4
Mos <acwzy@live.com>
2019-03-24 01:10:35 +0800

    feat: Finish [162.Find peak element](https://leetcode.com/problems/find-peak-element/) but ugly

---
+++ src/162.find-peak-element.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=162 lang=javascript
+ *
+ * [162] Find Peak Element
+ *
+ * https://leetcode.com/problems/find-peak-element/description/
+ *
+ * algorithms
+ * Medium (40.67%)
+ * Total Accepted:    222.9K
+ * Total Submissions: 545.4K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * A peak element is an element that is greater than its neighbors.
+ *
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element
+ * and return its index.
+ *
+ * The array may contain multiple peaks, in that case return the index to any
+ * one of the peaks is fine.
+ *
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index
+ * number 2.
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 1 or 5
+ * Explanation: Your function can return either index number 1 where the peak
+ * element is 2,
+ * or index number 5 where the peak element is 6.
+ *
+ *
+ * Note:
+ *
+ * TODO: Your solution should be in logarithmic complexity.
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  if (nums.length === 0) {
+    return -1;
+  }
+  if (nums.length === 1) {
+    return 0;
+  }
+  if (nums.length === 2) {
+    return nums[0] > nums[1] ? 0 : 1;
+  }
+  let index = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0 && nums[i] > nums[i + 1]) {
+      index = i;
+      break;
+    }
+    if (i === nums.length - 1 && nums[i] > nums[i - 1]) {
+      index = i;
+      break;
+    }
+    if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+};
+module.exports = findPeakElement;
